66b788b4fd275fec0df6cb5e2a2aa6655dec08c8
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-10-15 19:14:20 -0400

    adds migrations

---
+++ db/migrate/20211015230451_create_shows.rb
@@ -0,0 +1,16 @@
+class CreateShows < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.decimal :lat
+      t.decimal :lng
+      t.boolean :family
+      t.integer :price
+      t.booelan :musical
+      t.integer :mood_id
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
